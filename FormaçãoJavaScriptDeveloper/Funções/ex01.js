47f0350741c8ee2a626ce24eb95a4c8dac1abf4f
Bruno H C Jesus <149212642+AzGrimmm@users.noreply.github.com>
2024-01-15 17:20:23 -0300

    .

--- FormaçãoJavaScriptDeveloper/Funções/ex01.js
+++
@@ -1,8 +0,0 @@
-function incrementarJuros(valor,percentualJuros){
-    const valorDeAcrecimo = (percentualJuros /100) * valor
-    return valor + valorDeAcrecimo
-}
-
-console.log(incrementarJuros(100,10))
-console.log(incrementarJuros(100,15))
-console.log(incrementarJuros(100,20))
