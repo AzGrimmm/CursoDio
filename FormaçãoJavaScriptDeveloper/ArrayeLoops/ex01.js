d5666253c4fa48a424007e3933cca00913ae6f3b
Bruno H C Jesus <149212642+AzGrimmm@users.noreply.github.com>
2024-01-17 09:20:06 -0300

    .

---
+++ FormaçãoJavaScriptDeveloper/ArrayeLoops/ex01.js
@@ -0,0 +1,11 @@
+
+
+for (let i = 1; i <= 10; i++) {
+    var n = 5
+    let tab = `${n} * ${i} = ${n * i}`
+    console.log(tab) 
+}
+
+
+
+
---
+++ FormaçãoJavaScriptDeveloper/ArrayeLoops/ex02.js
@@ -0,0 +1,9 @@
+let listaNumeros = [0,1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i < listaNumeros.length; i++) {
+    const element = listaNumeros[i];
+    if(element % 2 == 0){
+        console.log(element)
+    }
+    
+}
